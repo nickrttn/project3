79fa05d5703b2bfa44e4663989c80d867a8d1c57
Nick Rutten <nick@sparrowinspace.com>
2015-06-21 00:12:32 +0200

    Finish user signup

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,7 +9,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   	assert_select "a[href=?]", help_path
   	assert_select "a[href=?]", signup_path
   	get signup_path
-  	assert_select "title", full_title("Sign up")
+  	assert_select "title", full_title("Je naam")
   end
 
 end
