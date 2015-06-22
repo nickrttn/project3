fa628ed1fce2ee4155be7e3b1b529909a904ee5b
Nick Rutten <nick@sparrowinspace.com>
2015-06-22 10:54:57 +0200

    Finished name entry & started main game logic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,16 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+WebFontConfig = {
+  google: { families: [ 'Roboto:400,700:latin', 'Roboto+Mono::latin' ] }
+};
+
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
